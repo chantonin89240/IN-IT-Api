4037ea7ff8addb451d3d13bd237917a4ae0d878e
AlvesLucas5483 <90681993+AlvesLucas5483@users.noreply.github.com>
2022-10-06 13:25:50 +0200

    création et utilisation d'un security router

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response, urlencoded } from 'express';
 import dotenv from 'dotenv';
+import * as securityRouter from "./src/route/User/SecurityRouter"
 import {resourcesRouter} from "./src/route/Resouces/ResourcesRouter";
 import {resourceRouter} from "./src/route/Resouces/ResourceRouter";
 import {typeRouter} from "./src/route/Types/TypeRouter";
 import {optionRouter} from "./src/route/Option/OptionRouter"
-import UserService from "./src/service/User/UserService"
+
 
 dotenv.config();
 
@@ -18,12 +19,11 @@ app.get("", (req: Request, res: Response) => res.send({message : "In-It API is r
   
   // Security functions. Comment and uncomment as needed for testing. 
   // Remember to uncomment and test routes. Tokens last 12 hours.
-app.post("/login", (req, res) => UserService.Authenticate(req, res))
-app.use((req, res, next) => UserService.Verify(req, res, next))
+  app.use("/login",securityRouter.Routes)
+  app.use(securityRouter.Securize)          //This ensures token presence for routes below.
 
   // Data acquirement routes go here. These will be secure.
-app.get("", (req: Request, res: Response) => res.send());
-app.use("/resources", resourcesRouter);
+app.use("/resources", resourcesRouter)
 app.use("/types", typeRouter)
 app.use("/options", optionRouter)
 app.use("/resource", resourceRouter)
